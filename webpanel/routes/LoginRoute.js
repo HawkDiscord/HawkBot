5d9191a011dabcb04321447c1e4a62d5294aba85
ForYaSee <yannisee@web.de>
2018-06-24 21:50:54 +0200

    Login & Logout + Basic Dashboard Route

--- webpanel/routes/LoginRoute.js
+++ webpanel/routes/LoginRoute.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const btoa = require('btoa');
+const superagent = require('superagent');
+const discordOauth = require('../entities/DiscordOauth.js');
+const webhook = require('../../util/webhook.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendStatus(200);
+    let redirect = encodeURIComponent(`${process.config.webpanel.baseurl}/login/callback`);
+    res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${process.config.bot.id}&redirect_uri=${redirect}&response_type=code&scope=identify%20guilds`);
+});
+
+router.get('/callback', async (req, res) => {
+    if(!req.query.code)
+        return res.redirect(process.config.webpanel.baseurl);
+    let code = req.query.code;
+    let credentials = btoa(`${process.config.bot.id}:${process.config.bot.secret}`);
+    let response = await superagent.post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(process.config.webpanel.baseurl)}/login/callback`)
+        .set('Authorization', `Basic ${credentials}`)
+        .send();
+    let loginData = response.body;
+    res.cookie('hawkToken', loginData.access_token);
+    let user = await discordOauth.fetchUser(loginData.access_token);
+    webhook.sendWebpanelLogin(user);
+    res.redirect(`${process.config.webpanel.baseurl}/dashboard`);
 });
 
 module.exports = router;
