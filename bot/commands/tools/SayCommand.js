cd1ca2a19864cf9cf47a70e79278ee2c2d253655
リー <meluten@gmail.com>
2018-07-01 11:40:30 +0200

    Schlaubi dev (#9)
    * Added Usage
    
    -Added USAGE correctly
    I was not clever enough to check how to add it 
    correctly (facepalm)
    
    * Update SayCommand.js
    
    Edited usage because @leedjd had worries that some users don't check that an embed title should be provided as text
    
    * Update SayCommand.js
    
    - Now it should be unpossible to not know what [#channel] pareter means

--- bot/commands/tools/SayCommand.js
+++ bot/commands/tools/SayCommand.js
@@ -15,17 +15,24 @@ class SayCommand extends Command {
             name: 'say',
             displayName: 'Say',
             description: 'Send messages to channels with Hawk',
+            usages: [
+                
+                {
+                    usage: '[#channel] <text>',
+                    description: 'Sends a normal text message'
+                },
+                {
+                    usage: '[#channel] [-title <text> | -description <text> | -color <HEX color code> | -footer <footer>]',
+                    description: 'Sends an embed with all options you specified'
+                }
+            ],
             path: __filename
         });
     }
 
     async run(message, args, lang) {
         if(args.length == 0) {
-            return message.channel.createMessage(lang.say.usage).then((message) => {
-                setTimeout(() => {
-                    message.delete();
-                }, 1500);
-            });
+            return this.sendHelp(message, lang);
         }
         let channelId;
         if(message.channelMentions.length === 0){
